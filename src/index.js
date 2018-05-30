b9a73a2fcddadeb59ad04ed2cf0935fd25a0d595
Matheus Sampaio <matheus@sampaio.us>
2018-05-30 18:31:15 -0500

    init

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const github = require('octonode')
+const dotenv = require('dotenv')
+const nconf = require('nconf')
+
+main()
+
+function main() {
+  initEnvironmentVariables()
+
+  const { GH_TOKEN, GH_REPOSITORY } = process.env
+
+  const client = github.client(GH_TOKEN)
+  const pr = client.pr(GH_REPOSITORY, 2)
+
+  // pr.createReviewRequests(['moisesflores22'], (...args) => console.log(...args))
+}
+
+function initEnvironmentVariables() {
+  dotenv.config()
+
+  nconf.argv()
+    .env()
+    .required(['GH_TOKEN', 'GH_REPOSITORY'])
+}
+
