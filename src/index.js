29f3892fbe333a44aace4908ca1730090023ac3d
Matheus Sampaio <matheus1401@gmail.com>
2018-10-11 19:49:14 -0500

    chore: update robots to app

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const commands = require('probot-commands')
 
 const handlePullRequests = require('./core')
 
-module.exports = robot => {
-  robot.on(['pull_request.opened', 'pull_request.reopened'], handlePullRequests)
+module.exports = app => {
+  app.on(['pull_request.opened', 'pull_request.reopened'], handlePullRequests)
 
-  commands(robot, 'review', handlePullRequests)
+  commands(app, 'review', handlePullRequests)
 }
